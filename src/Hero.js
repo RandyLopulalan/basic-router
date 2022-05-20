a4f2dd6e071a2f27c375ba57479692d76ca95ad7
Lopulalan Randy Yonathan <randylopulalan07@gmail.com>
2022-05-20 16:11:09 +0800

    update nav

--- src/Hero.js
+++ src/Hero.js
@@ -3,21 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About, ErrorPage, Footer, Header, Home, Profile } from "./Pages";
 
 const Hero = () => {
-  const [username, setUsername] = useState();
-  const [active, setActive] = useState("home");
+  const [username, setUsername] = useState()
+  const [active, setActive] = useState("home")
   return (
     <Router>
-      <Header active={active} setActive={setActive} />
+      <Header active={active} setActive={setActive}/>
       <Routes>
-        <Route
-          path="/"
-          element={<Home setUsername={setUsername} setActive={setActive} />}
-        />
-        <Route path="/about/:id" element={<About username={username} />} />
-        <Route
-          path="/profile/:id"
-          element={<Profile username={username} setActive={setActive} />}
-        />
+        <Route path="/" element={<Home setUsername={setUsername} setActive={setActive}/>} />
+        <Route path="/about/" element={<About/>}/>
+        <Route path="/about/:id" element={<About username={username}/>}/>
+        <Route path="/profile/" element={<Profile setActive={setActive}/>} />
+        <Route path="/profile/:id" element={<Profile username={username} setActive={setActive}/>} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
